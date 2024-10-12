e9ddc162f217473d40be4782a98b25c2fbaa43e3
brunosimon <simon.bruno.77@gmail.com>
2024-10-12 09:38:33 +0200

    :tada: Initialize

---
+++ vite.config.js
@@ -0,0 +1,20 @@
+import restart from 'vite-plugin-restart'
+
+export default {
+    root: 'sources/', // Sources files (typically where index.html is)
+    publicDir: '../static/', // Path from "root" to static assets (files that are served as they are)
+    server:
+    {
+        host: true, // Open to local network and display URL
+    },
+    build:
+    {
+        outDir: '../dist', // Output in the dist/ folder
+        emptyOutDir: true, // Empty the folder first
+        sourcemap: true // Add sourcemap
+    },
+    plugins:
+    [
+        restart({ restart: [ '../static/**', ] }) // Restart server on static file change
+    ],
+}
