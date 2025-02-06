6090c1b2e47ab3ca9c87274c9277d97a34d9a8b0
brunosimon <simon.bruno.77@gmail.com>
2025-02-06 18:01:57 +0100

    :sparkles: Rocks > Add moss

--- sources/Game/World/Scenery.js
+++ sources/Game/World/Scenery.js
@@ -1,4 +1,7 @@
+import * as THREE from 'three/webgpu'
 import { Game } from '../Game.js'
+import { color, dot, Fn, max, mix, normalGeometry, smoothstep, uniform, vec3, vec4 } from 'three/tsl'
+import { normalWorld } from 'three/tsl'
 
 export class Scenery
 {
@@ -6,8 +9,48 @@ export class Scenery
     {
         this.game = Game.getInstance()
 
+        if(this.game.debug.active)
+        {
+            this.debugPanel = this.game.debug.panel.addFolder({
+                title: '🛋️ Scenery',
+                expanded: true,
+            })
+        }
+
+        this.setStoneMaterial()
+
         this.setStatic()
         this.setDynamics()
+    }
+
+    setStoneMaterial()
+    {
+        const test = new THREE.MeshLambertNodeMaterial()
+        test.shadowSide = THREE.BackSide
+
+        const stoneColor = uniform(color('#ffe9d7'))
+        const mossColor = uniform(color('#d4d56d'))
+
+        test.outputNode = Fn(() =>
+        {
+            const upDot = dot(vec3(0.2, 1, 0.4).normalize(), normalGeometry).toVar()
+
+            const baseColor = mix(stoneColor, mossColor, max(0, upDot))
+            return this.game.lighting.lightOutputNodeBuilder(baseColor, this.game.lighting.addTotalShadowToMaterial(test))
+        })()
+        this.game.materials.save('stoneWhite', test)
+
+        // Debug
+        if(this.game.debug.active)
+        {
+            const debugPanel = this.debugPanel.addFolder({
+                title: 'Stone material',
+                expanded: true,
+            })
+
+            this.game.debug.addThreeColorBinding(debugPanel, stoneColor.value, 'stoneColor')
+            this.game.debug.addThreeColorBinding(debugPanel, mossColor.value, 'mossColor')
+        }
     }
 
     setStatic()
